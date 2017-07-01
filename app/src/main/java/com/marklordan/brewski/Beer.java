4d7f3458d45f3ed17156cf7d8a5c7bfdcf8db7ca
DarkNormal <mark.lordan@me.com>
2017-07-01 11:42:27 +0100

    Retrofit implementation for pulling down a list of beers JSON being deserialized with gson to POJOs with brewery info included Refreshing the recyclerview datasource to display info

--- app/src/main/java/com/marklordan/brewski/Beer.java
+++ app/src/main/java/com/marklordan/brewski/Beer.java
@@ -1,16 +1,35 @@
 package com.marklordan.brewski;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Beer model class
  */
 
 public class Beer {
 
-    private String mTitle, mBreweryName;
+    @SerializedName("name")
+    private String mTitle;
+    @SerializedName("description")
+    private String mDescription;
 
-    public Beer(String beerName, String beerBrewery){
-        mTitle = beerName;
-        mBreweryName = beerBrewery;
+    @SerializedName("abv")
+    private double mAbv;
+
+    @SerializedName("breweries")
+    private Brewery[] brewery;
+
+    private String mBreweryName;
+
+    public Beer(String mTitle, String mBreweryName) {
+        this.mTitle = mTitle;
+        this.mBreweryName = mBreweryName;
+    }
+
+    public Beer(String mTitle, String mDescription, String mBreweryName) {
+        this.mTitle = mTitle;
+        this.mDescription = mDescription;
+        this.mBreweryName = mBreweryName;
     }
 
     public String getBeerTitle() {
---
+++ app/src/main/java/com/marklordan/brewski/Brewery.java
@@ -0,0 +1,29 @@
+package com.marklordan.brewski;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Mark on 01/07/2017.
+ */
+
+public class Brewery {
+
+    @SerializedName("name")
+    public String mName;
+
+    @SerializedName("description")
+    public String mDescription;
+
+    @SerializedName("established")
+    public int mYearEstablished;
+
+    @SerializedName("website")
+    public String mWebsite;
+
+    @SerializedName("status")
+    public String mIsVerified;
+
+    public String getmName() {
+        return mName;
+    }
+}
